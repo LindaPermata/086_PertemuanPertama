189c41e86ffa99427348ac190e029336fdf00933
LindaPermata <permatalinda531@gmail.com>
2024-02-26 15:16:29 +0700

    Add project files.

---
+++ PertemuanPertama/PertemuanPertama.cpp
@@ -0,0 +1,34 @@
+// PertemuanPertama.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{//begin
+     // numeric nPanjang, nLebar, nLuas
+	int nPanjang, nLebar, nLuas;
+	// display 'Masukan Panjang= '
+	cout << "Masukan Panjang = ";
+	// accept nPanjang
+	cin >> nPanjang;
+	// display 'Masukan Lebar = '
+	cout << "masukan Lebar = ";
+	// accept nLuas
+	cin >> nLebar;
+	// compute nLuas = nPanjang * nLebar
+	nLuas = nPanjang * nLebar;
+	// display 'Luasnya adalah = ' + nLuas
+	cout << "Luasnya adalah = " << nLuas;
+}//end 
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
